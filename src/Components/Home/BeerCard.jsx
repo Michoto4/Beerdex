fb0f86a1db97825a9787e0c880b4bf5140b21a1c
Michoto4 <ziemniaczek230@gmail.com>
2024-08-24 21:53:49 +0200

    scalable frontend beerdex homepage

---
+++ src/Components/Home/BeerCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import beerDefault from '../../assets/beerDefault.jpg'
+import styles from './Home.module.css';
+
+function BeerCard() {
+  return (
+    <div className={styles.beerContainer}>
+        <div className={styles.beerImage}>
+            <label htmlFor="beerImage">
+                <img src={beerDefault} alt="beerImage" />
+            </label>
+            
+            <input type="file" id='beerImage' name='beerImage' />
+        </div>
+        <div className={styles.middleContainer}>
+            <h2>Example Beer Name</h2>
+            <h4>Example Beer Type/Taste</h4>
+            <hr />
+            <p>This is an example description of a beer that you would add here to describe your beer.</p>
+        </div>
+        <div className={styles.rightContainer}>
+            <h5>Rating:</h5>
+            <p>5/10</p>
+            <h5>Date Added:</h5>
+            <p>25.08.2024</p>
+        </div>
+    </div>
+  )
+}
+
+export default BeerCard
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,80 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from './Home.module.css';
-import '../../App.css';
-import avatar from '../../assets/default.jpg';
 import toast, { Toaster } from 'react-hot-toast';
-import { useFormik } from 'formik';
-import convertToBase64 from '../../helper/convert';
-import useFetch from '../../hooks/fetch.hook';
-import { updateUser } from "../../helper/helper";
-import { useNavigate } from 'react-router-dom';
+import avatar from '../../assets/default.jpg';
+import beerDefault from '../../assets/beerDefault.jpg';
+import BeerCard from './BeerCard.jsx';
 
-function Home(){
+// import FontAwesome icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
-    const [file, setFile] = useState();
-    const [{ isLoading, apiData, serverError }] = useFetch()
-    const navigate = useNavigate()
-
-    // check if user is logged in (by checking if there is a jwt token saved in your browser's local storage)
-    // if isn't then block access by navigating to '/' page
-    let checkToken = localStorage.getItem('token');
-    if(!checkToken){
-        useEffect(() => {
-            navigate('/');
-        });
-    }
-
-
-    const formik = useFormik({
-        initialValues : {
-            profile: ''
-        },
-        onSubmit : async values => {
-            values = await Object.assign(values, {profile: file || ''})
-
-            let updatePromise = updateUser(values)
-            toast.promise(updatePromise, {
-                loading: 'Updating...',
-                success : 'Update Successfull!',
-                error : 'Could not update your profile.'
-            })
-        }
-    });
-
-
-      /** use this to convert uploaded photo to base64 */
-    const onUpload = async e => {
-        const base64 = await convertToBase64(e.target.files[0]);
-        setFile(base64)
-        
-    }
-
-  // logout handler function
-    function userLogout(){
-        localStorage.removeItem('token');
-        navigate('/')
-    }
-
-
-    return(
+function Home() {
+  return (
         <div className={styles.container}>
             <Toaster position="top-center" reverseOrder={false}></Toaster>
-            <form className={styles.form} onSubmit={formik.handleSubmit}>
-                <h3>Welcome to Beerdex, <b>{apiData?.username}</b></h3>
-
-                <div className={styles.profileContainer}>
-                    <label htmlFor="profile">
-                        <img src={file || apiData?.profile || avatar} className={styles.profilePhoto} alt="avatar" />
-                    </label>
-                    
-                    <input onChange={onUpload} type="file" id='profile' name='profile' />
+            <form className={styles.form}>
+                <div className={styles.topBar}>
+                    <h1>BEERDEX</h1>
+                    <h3>Your beer pokedex</h3>
+                    <div className={styles.profileContainer}>
+                        <label htmlFor="profile">
+                            <img src={avatar} alt="avatar" />
+                        </label>
+                        
+                        <input type="file" id='profile' name='profile' />
+                    </div>
                 </div>
-                <button className={styles.submitUpdate} type="submit">SAVE CHANGES</button><br />
-                <button className={styles.logOutButton} onClick={userLogout}>LOG OUT</button>
+                <hr />
+                <input className={styles.beerBrowseInput} type="text" placeholder="Search for a Beer" />
+                <div className={styles.appContainer}>
+                    <BeerCard></BeerCard>
+                    <BeerCard></BeerCard>
+                    <BeerCard></BeerCard>
+                    <BeerCard></BeerCard>
+                    <BeerCard></BeerCard>
+                    <BeerCard></BeerCard>
+                    <BeerCard></BeerCard>
+                </div>
             </form>
         </div>
-
-    )
+  )
 }
 
 export default Home
