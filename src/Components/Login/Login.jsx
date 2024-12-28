34f1bd359e32f26e359427e7553391794d358c8e
Michoto <ziemniaczek230@gmail.com>
2024-12-29 00:09:41 +0100

    hotfix

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from './Login.module.css';
 import { Link, useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
